c2d4d5101972bf00abaa29337630b210aa7c65d0
Vyacheslav Melnikov <robber2005@mail.ru>
2026-03-20 19:43:13 +0300

    Fix transferred method in helpers.py

--- data.py
+++ data.py
@@ -1,6 +1,3 @@
-import random
-import string
-
 BASE_URL = "https://stellarburgers.education-services.ru"
 
 class Endpoints:
@@ -9,11 +6,3 @@ class Endpoints:
     USER = f"{BASE_URL}/api/auth/user"
     ORDERS = f"{BASE_URL}/api/orders"
     INGREDIENTS = f"{BASE_URL}/api/ingredients"
-
-def generate_user_payload():
-    random_str = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
-    return {
-        "email": f"tester_{random_str}@yandex.ru",
-        "password": f"pass_{random_str}",
-        "name": f"Tester_{random_str}"
-    }
