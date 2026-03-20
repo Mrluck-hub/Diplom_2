c65fc2d736528b369f10a160fd4c77970d4e9509
Vyacheslav Melnikov <robber2005@mail.ru>
2026-03-20 19:44:13 +0300

    Fix test

--- tests/test_order.py
+++ tests/test_order.py
@@ -5,10 +5,8 @@ from api_client import BurgerApiClient
 class TestOrders:
     @allure.title("Создание заказа с авторизацией")
     def test_order_with_auth(self, created_user):
-        res_reg, _ = created_user
-        token = res_reg.json()["accessToken"]
+        token = created_user["token"]
         ing = BurgerApiClient.get_ingredients().json()["data"][0]["_id"]
-        
         res = BurgerApiClient.create_order({"ingredients": [ing]}, token)
         assert res.status_code == 200
         assert res.json()["success"] is True
@@ -21,22 +19,18 @@ class TestOrders:
 
     @allure.title("Создание заказа с ингредиентами")
     def test_order_with_ingredients(self, created_user):
-        res_reg, _ = created_user
-        token = res_reg.json()["accessToken"]
+        token = created_user["token"]
         ings = [i["_id"] for i in BurgerApiClient.get_ingredients().json()["data"][:2]]
-        
         res = BurgerApiClient.create_order({"ingredients": ings}, token)
         assert res.status_code == 200
 
     @allure.title("Заказ без ингредиентов")
     def test_order_no_ing_fail(self, created_user):
-        res_reg, _ = created_user
-        res = BurgerApiClient.create_order({"ingredients": []}, res_reg.json()["accessToken"])
+        res = BurgerApiClient.create_order({"ingredients": []}, created_user["token"])
         assert res.status_code == 400
         assert res.json()["message"] == "Ingredient ids must be provided"
 
     @allure.title("Заказ с неверным хешем")
     def test_order_bad_hash_fail(self, created_user):
-        res_reg, _ = created_user
-        res = BurgerApiClient.create_order({"ingredients": ["not_a_hash"]}, res_reg.json()["accessToken"])
+        res = BurgerApiClient.create_order({"ingredients": ["not_a_hash"]}, created_user["token"])
         assert res.status_code == 500
