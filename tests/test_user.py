522ea3a5cce2c34eef96ffd78d094eed1fa2972f
Vyacheslav Melnikov <robber2005@mail.ru>
2026-03-20 19:44:42 +0300

    Fix tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,35 +1,40 @@
 import allure
 from api_client import BurgerApiClient
+from helpers import UserHelper
 
 @allure.feature("User")
 class TestUser:
     @allure.title("Создать уникального пользователя")
-    def test_create_user_success(self, user_data):
-        res = BurgerApiClient.register_user(user_data)
+    def test_create_user_success(self):
+        payload = UserHelper.generate_user_payload()
+        res = BurgerApiClient.register_user(payload)
         assert res.status_code == 200
         assert res.json()["success"] is True
         BurgerApiClient.delete_user(res.json().get("accessToken"))
 
     @allure.title("Создать дубликат пользователя")
     def test_create_duplicate_user(self, created_user):
-        _, data = created_user
-        res = BurgerApiClient.register_user(data)
+        payload = created_user["payload"]
+        res = BurgerApiClient.register_user(payload)
         assert res.status_code == 403
         assert res.json()["message"] == "User already exists"
 
     @allure.title("Создать пользователя без email")
     def test_create_user_no_field(self):
-        res = BurgerApiClient.register_user({"password": "123", "name": "Ivan"})
+        payload = UserHelper.generate_user_payload()
+        payload.pop("email")
+        res = BurgerApiClient.register_user(payload)
         assert res.status_code == 403
         assert "required fields" in res.json()["message"]
 
     @allure.title("Логин существующим пользователем")
     def test_login_success(self, created_user):
-        _, data = created_user
-        res = BurgerApiClient.login_user({"email": data["email"], "password": data["password"]})
+        payload = created_user["payload"]
+        res = BurgerApiClient.login_user({"email": payload["email"], "password": payload["password"]})
         assert res.status_code == 200
 
     @allure.title("Логин с неверными данными")
-    def test_login_wrong_creds(self):
-        res = BurgerApiClient.login_user({"email": "fail@ya.ru", "password": "000"})
+    def test_login_wrong_creds(self,created_user):
+        payload = created_user["payload"]
+        res = BurgerApiClient.login_user({"email": payload["email"], "password": "000"})
         assert res.status_code == 401
